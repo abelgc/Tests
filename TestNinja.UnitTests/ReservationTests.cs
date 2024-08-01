d592d6c3d0945192833d0ff70e695a9cca46512a
abelc <abel.canizares@derivco.co.uk>
2024-08-01 10:02:28 +0200

    Add test structure

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            //Act
+            var restult = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+            //Assert
+            //Assert.IsTrue(result);
+            restult.Should().Be(true);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCanellReservation_ReturnsTrue()
+        {
+            //Arrange
+            var madeByuser = new User();
+            var reservation = new Reservation { MadeBy = madeByuser };
+            //Act
+            var restult = reservation.CanBeCancelledBy(madeByuser);
+            //Assert
+            restult.Should().Be(true);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_DifferentUserCanellReservation_ReturnsFalse()
+        {
+            //Arrange
+            var madeByuser = new User();
+            var reservation = new Reservation { MadeBy = madeByuser };
+            //Act
+            var restult = reservation.CanBeCancelledBy(new User());
+            //Assert
+            restult.Should().NotBe(true);
+
+        }
+    }
+}
